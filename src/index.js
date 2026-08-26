13ed2bb856fb1f9b61161496397715747812ed7d
Gleb Cherepnin <gglebasemil@gmail.com>
2026-08-26 16:45:30 +0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const absoluteNumber = Math.abs(n);
+
+  let str = absoluteNumber.toString();
+
+  str = str.split('').reverse().join('');
+
+  const num = Number(str);
+
+  return num;
 };
